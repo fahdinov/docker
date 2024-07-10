680c9f684975799a828b3abbf6925d8e3402d3ee
fahdinov <fahd.ameur@gmail.com>
2024-07-10 10:40:42 +0200

    with simple data test

--- index.php
+++ index.php
@@ -6,9 +6,25 @@ $databasePassword = getenv("MARIADB_ROOT_PASSWORD");
 $databaseName = getenv("MARIADB_DATABASE");
 
 // Connect to the database
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 if($mysqli){
-    print("Connection Established Successfully");
+    $mysqli->query("DROP TABLE IF EXISTS test");
+    $mysqli->query("CREATE TABLE test(id INT)");
+    $mysqli->query("INSERT INTO test(id) VALUES (1), (2), (3)");
+
+    $result = $mysqli->query("SELECT id FROM test ORDER BY id ASC");
+
+    echo "Reverse order...\n";
+    for ($row_no = $result->num_rows - 1; $row_no >= 0; $row_no--) {
+        $result->data_seek($row_no);
+        $row = $result->fetch_assoc();
+        echo " id = " . $row['id'] . "\n";
+    }
+
+    echo "Result set order...\n";
+    foreach ($result as $row) {
+        echo " id = " . $row['id'] . "\n";
+    }
  }else{
     print("Connection Failed ");
  }

7dc9ed558e460ad2659b2298c4534b51119febc5
fahdinov <fahd.ameur@gmail.com>
2024-07-11 10:30:19 +0200

    app with fresh db

--- php-app/dbConnection.php
+++ php-app/dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'db';
-$databaseName = 'phpdb';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = getenv("MARIADB_HOST");
+$databaseUsername = getenv("MARIADB_ROOT_USER");
+$databasePassword = getenv("MARIADB_ROOT_PASSWORD");
+$databaseName = getenv("MARIADB_DATABASE");
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
